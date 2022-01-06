7067da5e6e6d1992878fe2aadc2a5f360824d7e4
Pau Garcia <pauligarcia_05@hotmail.com>
2022-01-06 18:05:20 -0300

    add first_component

---
+++ First_Component/machine.js
@@ -0,0 +1,9 @@
+class Machine extends React.Component {
+    render(){
+        return (
+        <div>
+        <p>{this.props.s1} {this.props.s2} {this.props.s3} </p>
+        </div>
+        )
+    }
+}
